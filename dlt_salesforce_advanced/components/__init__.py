f6cf432585402d5006c8f45350f67a49856f2315
Bertrand Leymarios <bertrand.leymarios@gmail.com>
2025-12-06 09:15:59 +0100

    clean useless __init__

--- dlt_salesforce_advanced/components/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Reusable pipeline components for DLT.
-
-This package contains modular components for building data pipelines:
-- salesforce_extid_resolver: Component to resolve an external id into a salesforce Id
-"""
-
-__version__ = "1.0.0"
